b9eef705c0a342e6b7e841fec7d8927a82e061c9
AnastasiaMorozovaBPI205 <avmorozova_6@edu.hse.ru>
2021-09-19 11:05:15 +0300

    1 point commit (added a settings button)

--- avmorozova_6PW2/ViewController.swift
+++ avmorozova_6PW2/ViewController.swift
@@ -11,7 +11,37 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupSettingsButton()
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+     return .lightContent
+     }
+    
+    private func setupSettingsButton()
+    {
+        let settingsButton = UIButton(type: .system)
+        settingsButton.setImage(UIImage(named: "setting_logo"), for: .normal)
+        
+        view.addSubview(settingsButton)
+        
+        settingsButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        settingsButton.topAnchor.constraint(
+         equalTo: view.safeAreaLayoutGuide.topAnchor,
+         constant: 15
+         ).isActive = true
+        
+        settingsButton.trailingAnchor.constraint(
+         equalTo: view.safeAreaLayoutGuide.trailingAnchor,
+         constant: -15
+         ).isActive = true
+        
+        settingsButton.heightAnchor.constraint(equalToConstant: 30).isActive
+        = true
+         settingsButton.widthAnchor.constraint(equalTo:
+        settingsButton.heightAnchor).isActive = true
     }
 
 
