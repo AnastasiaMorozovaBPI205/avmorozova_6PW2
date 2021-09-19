a372001824898a0fe469f604a3e7396ebf99204b
AnastasiaMorozovaBPI205 <avmorozova_6@edu.hse.ru>
2021-09-19 11:30:16 +0300

    2 points commit(added function - pressing settings button makes settings view to appear/disappear)

--- avmorozova_6PW2/ViewController.swift
+++ avmorozova_6PW2/ViewController.swift
@@ -9,15 +9,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private let settingsView = UIView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupSettingsView()
         setupSettingsButton()
     }
+    
+    @objc
+     private func settingsButtonPressed() {
+     UIView.animate(withDuration: 0.1, animations: {
+     self.settingsView.alpha = 1 - self.settingsView.alpha
+     })
+     }
     
     override var preferredStatusBarStyle: UIStatusBarStyle {
      return .lightContent
      }
+    
+    private func setupSettingsView()
+    {
+        settingsView.backgroundColor = .blue
+        view.addSubview(settingsView)
+        
+        settingsView.translatesAutoresizingMaskIntoConstraints = false
+        
+        settingsView.topAnchor.constraint(
+         equalTo: view.safeAreaLayoutGuide.topAnchor,
+         constant: 10
+         ).isActive = true
+        
+        settingsView.trailingAnchor.constraint(
+         equalTo: view.safeAreaLayoutGuide.trailingAnchor,
+         constant: -10
+         ).isActive = true
+        
+        settingsView.heightAnchor.constraint(
+            equalToConstant: 300
+        ).isActive = true
+        
+        settingsView.widthAnchor.constraint(
+            equalToConstant: 200
+        ).isActive = true
+        
+        settingsView.alpha = 0
+    }
     
     private func setupSettingsButton()
     {
@@ -42,6 +80,9 @@ class ViewController: UIViewController {
         = true
          settingsButton.widthAnchor.constraint(equalTo:
         settingsButton.heightAnchor).isActive = true
+        
+        settingsButton.addTarget(self, action: #selector(settingsButtonPressed),
+        for: .touchUpInside)
     }
 
 
